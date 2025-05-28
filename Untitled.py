95cc8360b3512b924e1bd72e4dd1e028b494a546
alessandroco <116315171+alessandroco@users.noreply.github.com>
2025-05-28 13:58:06 +0200

    Continued the Project / added comments
    Added ipynb for data exploration

--- Untitled.py
+++ Untitled.py
@@ -1,19 +1,33 @@
+# Importing the libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Reading the data
 data = pd.read_csv('housing.csv')
 
+# Displaying information about the dataset and checking for null values.
 data.info()
+#the dataset present ~ 200 null values
 
+# Dropping rows with missing values and saving the changes.
 data.dropna(inplace=True)
 
-from sklearn.model_selection import train_test_split
 
+# Splitting the data into X & Y data.
+    # X will be the same data frame but without the target Variable (Median House Value).
+        # The "Axis=1" command will make us drop the whole column.
+    # Y logically will be only the target variable (Median House Value).
+
+from sklearn.model_selection import train_test_split
 x = data.drop(['median_house_value'], axis=1)
 y = data['median_house_value']
 
-X_train, X_test, y_train, y_test = train_test_split(X,y test_size=0.2)
+# We'll now set the ratios of both the Learning data and the Testind data with an optional ratio of 0.2 of the data used for testing.
 
-train_data = X_train.join(y_train)
+X_train, X_test, y_train, y_test = train_test_split(x,y, test_size=0.2)
+
+# Now we'll join the 2 dataset 
+
+data
